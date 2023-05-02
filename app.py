5e028b5f03bcf856a8e2c3284eb76a8a25b97057
Enrique Mena-Camilo <kikemeca05@gmail.com>
2023-05-01 19:27:52 -0600

    Inferencia web lista
    Falta establecer situaciones específicas.
    Falta agregar estilos y validar datos.
    Falta validar funcionamiento en aws.
    Falta actualizar modelo de pycaret.

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_cio():
-    prediction = request.args.get("prediction", 3)
-    return render_template("index.html", prediction=int(prediction))
+    return render_template("index.html")
 
 
-@app.route("/predict")
+@app.route("/predict", methods=["POST"])
 def predict():
     import pandas as pd
     import numpy as np
     from pycaret.classification import load_model
 
-    cvf = request.args.get("cvf", 320)
-    edad = request.args.get("edad", 26)
-    sexo = request.args.get("sexo", 1)
-    ef = request.args.get("ef", 2)
+    cvf = request.json.get("cvf", None)
+    edad = request.json.get("age", None)
+    sexo = request.json.get("gender", None)
+    ef = request.json.get("smoke", None)
+    # TODO Validar los datos que llegan
 
     model = load_model("final_rf_poster_cio")
     data = pd.DataFrame(np.array([[int(cvf), np.nan, int(edad), int(sexo), int(ef), 5]]), 
                         columns=["CVF", "Porcentaje", "Edad", "Sexo", "Estado del fumador", "Semanas"])
     result = model.predict(data)[0]
 
-    return redirect(url_for("hello_cio", prediction=result))
+    return {"prediction": int(result)}, 200
 
 
 
