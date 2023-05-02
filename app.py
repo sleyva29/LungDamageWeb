6354ef00c6ec9d5626657384696ebec7804776ac
Enrique Mena-Camilo <kikemeca05@gmail.com>
2023-05-01 18:03:26 -0600

    Inferencia en web

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_cio():
-    return render_template("index.html")
+    prediction = request.args.get("prediction", 3)
+    return render_template("index.html", prediction=int(prediction))
+
+
+@app.route("/predict")
+def predict():
+    import pandas as pd
+    import numpy as np
+    from pycaret.classification import load_model
+
+    cvf = request.args.get("cvf", 320)
+    edad = request.args.get("edad", 26)
+    sexo = request.args.get("sexo", 1)
+    ef = request.args.get("ef", 2)
+
+    model = load_model("final_rf_poster_cio")
+    data = pd.DataFrame(np.array([[int(cvf), np.nan, int(edad), int(sexo), int(ef), 5]]), 
+                        columns=["CVF", "Porcentaje", "Edad", "Sexo", "Estado del fumador", "Semanas"])
+    result = model.predict(data)[0]
+
+    return redirect(url_for("hello_cio", prediction=result))
+
 
 
 if __name__ == "__main__":
