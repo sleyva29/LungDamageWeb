739e67331a8cf66e723e5423610390ab8fb90b25
EnriqueMC557 <kikemeca05@gmail.com>
2023-05-13 21:51:46 -0600

    Invalid selection alert ready

--- static/js/utils.js
+++ static/js/utils.js
@@ -9,12 +9,21 @@ async function getData(url, data) {
 }
 
 function predict(){
+    let valid_cvf = document.getElementById("cvf").checkValidity();
+    let valid_age = document.getElementById("age").checkValidity();
     let cvf = document.getElementById("cvf").value;
     let age = document.getElementById("age").value;
     let gender = document.getElementById("gender").value;
     let smoke = document.getElementById("smoke").value;
     let url = "/predict";
-    // TODO Validar rangos de datos
+
+    if (!valid_cvf || !valid_age || gender == 0 || smoke == 0){
+        alert("Por lo menos un campo no es válido, por favor verifique los datos ingresados.");
+        return;
+    }
+
+    gender = gender - 1;
+    smoke = smoke - 1;
 
     getData(url, {cvf: cvf, age: age, gender: gender, smoke: smoke})
         .then(response => response.json()
